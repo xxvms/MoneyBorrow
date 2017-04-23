7c07d86ea094449bce2b59f73dbabeb95d420a88
Tom Breza <tom@pcservicegroup.co.uk>
2017-04-23 11:21:31 +0100

    spelling corrections

--- MoneyBorrow/MoneyBorrow.cpp
+++ MoneyBorrow/MoneyBorrow.cpp
@@ -2,11 +2,12 @@
 //
 
 #include <iostream>
-double moneyIntrestFunction(double money_, double percent_);
+double moneyInterestFunction(double money_, double percent_);
 
 
 int main()
 {
+	// user prvides deails for the caluclation
 	std::cout << "Enter initial amount: ";
 	double money;
 	std::cin >> money;
@@ -17,17 +18,18 @@ int main()
 	double percent;
 	std::cin >> percent;
 	
-	system("cls");
+	system("cls"); // clean screan
 	
+	//confirmation of the user choices
 	std::cout << "Confirmation of your choices: " << std::endl;
 	std::cout << "Deposit: " << money << std::endl;
 	std::cout << "Period: " << years << std::endl;
-	std::cout << "Intrest: " << percent << std::endl;
+	std::cout << "Interest: " << percent << std::endl;
 
 
-	for (int i = 0; i < years; i++)
+	for (int i = 0; i < years; i++) // loop for increasing amount for every year
 	{
-		double totalMoney = moneyIntrestFunction(money, percent);
+		double totalMoney = moneyInterestFunction(money, percent); // function call
 
 		std::cout << "Money after " << i + 1 << " year " << totalMoney << std::endl;
 		money = totalMoney;
@@ -38,7 +40,7 @@ int main()
 	system("pause");
     return 0;
 }
-double moneyIntrestFunction(double money_, double percent_)
+double moneyInterestFunction(double money_, double percent_) // function returning double, with two variables size of deposit and interest
 {
 	double total;
 	total = ((money_ / 100) * percent_) + money_;
